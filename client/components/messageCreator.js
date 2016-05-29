7b87193c19192062d891368adc1d2f5db68f2f7a
Andrew Patipaksiri <apax714@gmail.com>
2016-05-29 12:09:16 -0700

    [setup] adds components to be connected to serverside

--- client/components/messageCreator.js
+++ client/components/messageCreator.js
@@ -1,39 +1,70 @@
 import React,{ Component, PropTypes } from 'react';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import socket from 'socket.io-client';
+import profile from './profile';
 
-let enterKeyCode = 13;
+
 class MessageCreator extends Component {
 
-  constructor(props){
-    super(props);
-    this.state= {text: ''};
+	 constructor(props, context) {
+    super(props, context);
+    this.state = {
+      text: '',
+      typing: false
+    };
+		this.socket = io();
   }
 
+
+
+  onKeyDown(event) {
+  	console.log('my id: ', profile.id);
+    const { message , typing} = this.props;
+    console.log('the this props ,: ', this.props);
+    const text = event.target.value.trim();
+    if (event.which === 13) {
+      event.preventDefault();
+      var data = {
+        message: text,
+        user: id,
+        time: moment.utc().format('lll')
+      };
+      // socket.emit('codeChange', data);
+      // socket.emit('stop typing', { user: user.username, channel: activeChannel });
+      console.log('the data: ' , data);
+      
+      this.setState({ text: '', typing: false });
+    }
+  }
+  onChange(event) {
+    const { userId , typing } = this.props;
+    this.setState({ text: event.target.value });
+    if (event.target.value.length > 0 && !this.state.typing) {
+      console.log('i am typing : ', event.target.value)
+      this.setState({ typing: true});
+    }
+    if (event.target.value.length === 0 && this.state.typing) {
+      // socket.emit('stop typing', { user: user.username, channel: activeChannel });
+      this.setState({ typing: false});
+    }
+  }
   render() {
     return (
       <textarea 
         name="message"
         className="input"
         value={this.state.text}
-        handleInputChange={this.handleInputChange.bind(this)}
-        handleInputKeyDown={this.handleInputKeyDown.bind(this)}
+        onChange={this.onChange.bind(this)}
+        onKeyDown={this.onKeyDown.bind(this)}
       />
     );
   }
-   handleInputChange(event, value) {
-    this.setState({text: event.target.value});
-  }
-  handleInputKeyDown(event) {
-    // keyboard code for Enter key
-    if(event.keyCode === 13) {
-      console.log(event);
-      let text = this.state.text.trim();
-      if(text) {
-        console.log(this);
-      }
-      this.setState({text: ''});
-    }
-  }
+
+}
+function mapStateToProps(state){
+	return { message: state.text}
 }
 
-
-export default MessageCreator;
+export default connect(mapStateToProps)(MessageCreator);
