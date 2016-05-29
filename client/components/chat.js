fcfb160b0e5259b9e0b06c393f23b13effb4c52b
Andrew Patipaksiri <apax714@gmail.com>
2016-05-28 21:53:48 -0700

    [fix] separates message creator from chat container

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,64 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
-import Draggable, { DraggableCore } from 'react-draggable';
-import MessageList from './messageList';
-import * as actions from '../actions';
+	import React, { Component } from 'react';
+	import { connect } from 'react-redux';
+	import { Link } from 'react-router';
+	import Draggable, { DraggableCore } from 'react-draggable';
+	import MessageList from './messageList';
+	import MessageCreator from './messageCreator';
+	import * as actions from '../actions';
 
-export default class Chat extends Component {
-  constructor(props){
-    super(props);
-    // this.state= {
-    //   createMessage: { fromID, toID, message }
-    // }
-    this.state = {
-      text: ''
-    }
-    console.log(this.props);
-  }
+	export default class Chat extends Component {
 
-
-  render() {
-    return (
-      <Draggable>
-          <div className="wrapper">
-            <nav id="nav" className="nav">
-              <div className="default-nav">
-                <div className="main-nav">
-                  <div className="toggle"></div>
-                  <div className="main-nav-item"><a href="#" className="main-nav-item-link">Andrew</a></div>
-                  <div className="options"></div>
-                </div>
-              </div>
-            </nav>
-            <div id="inner" className="inner">
-              <div id="content" className="content">
-                <MessageList />
-              </div>
-            
-            </div>
-            <div id="bottom" className="bottom">
-              bottom
-              <textarea id="input" name="message" onChange={this.onChange.bind(this)} onKeyDown= {this.onKeyDown.bind(this)} className="input"></textarea>
-              <div id="send" className="send">Button</div>
-            </div>
-          </div>
-      </Draggable>
-    )
-  }
-  onChange(event, value) {
-    this.setState({text: event.target.value});
-
-  }
-  onKeyDown(event) {
-    // keyboard code for Enter key
-    if(event.keyCode === 13) {
-      console.log(this.props);
-      let text = this.state.text.trim();
-      if (text) {
-       console.log( this.props);
-      }
-      this.setState({text: ''});
-    }
-  }
+	render() {
+	return (
+	<Draggable>
+			<div className="wrapper">
+			<nav id="nav" className="nav">
+			<div className="default-nav">
+			<div className="main-nav">
+			<div className="toggle"></div>
+			<div className="main-nav-item"><a href="#" className="main-nav-item-link">Andrew</a></div>
+			<div className="options"></div>
+			</div>
+			</div>
+			</nav>
+			<div id="inner" className="inner">
+			<MessageList />
+				</div>
+			<div id="bottom" className="bottom">
+			<MessageCreator />
+			<div id="send" className="send"></div>
+			</div>
+			</div>
+	</Draggable>
+	)
+	}
 }
